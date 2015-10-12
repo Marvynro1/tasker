2e87f808b2c42a884e4154cbd686892586a41aa8
Marvyn Orellana <marvynro1@gmail.com>
2015-10-12 21:02:17 +0000

    Added bootstrap, simple_form, created models, migrated database

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Tasker::Application.routes.draw do
-
-  root 'tasks#index'
+  resources :items
+  root 'items#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151011040315) do
+ActiveRecord::Schema.define(version: 20151012192134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tasks", force: true do |t|
     t.string   "title"
